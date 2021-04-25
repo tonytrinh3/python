259ce4a2c16931721bf86d6ed10c5ca5cfdb8ceb
Tony Trinh <tony.w.trinh3@gmail.com>
2021-04-24 19:14:56 -0700

    second pro cont

--- My_Django_Stuff/second_project/second_project/urls.py
+++ My_Django_Stuff/second_project/second_project/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from first_app import views
 
 urlpatterns = [
-    url('',views.index,name="index"),
+    path('', views.index, name="index"),
     path('admin/', admin.site.urls),
-    path ('first_app/',include('first_app.urls'))
+    path ('first_app/', include('first_app.urls'))
 ]
